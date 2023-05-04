a5452cc919d1755f8f7654fc5071b8a9758965d4
Davinciam1 <ruslananushko2113@gmail.com>
2023-05-04 16:23:30 +0300

    established connection between Api and database, started to create  tables in datadase

--- Api_connect.py
+++ Api_connect.py
@@ -8,6 +8,16 @@ from datetime import datetime
 from tqdm import tqdm
 
 
+def json_to_columns(row):
+    # преобразование JSON в словарь
+    data = row[0]
+    # создание новых столбцов для каждого ключа в JSON
+    temp_row = {}
+    for key in data:
+        temp_row['items.' + key] = data[key]
+    return pd.Series(temp_row)
+
+
 class Ysell_regu:
 
     def __init__(self):
@@ -19,14 +29,59 @@ class Ysell_regu:
             'accept': '*/*',
             'Authorization': token
         }
-        self.dates = []
+        self.pages = None
         self.temp_frame = pd.DataFrame()
 
-    def orders_req(self,page=1):
-        sort='?sort=-id&'
-        pages='page='+str(page)
-        url=self.url+'order'+sort+pages
-        response = requests.post(url=url, headers=self.headers)
+    def orders_by_page(self, page=1):
+        sort = '?sort=-id&'
+        pages = 'page=' + str(page)
+        url = self.url + 'order' + sort + pages
+        response = requests.get(url=url, headers=self.headers)
         print(response.status_code)
         json_data = response.json()
         df = pd.json_normalize(json_data)
+
+        # применение функции к датафрейму
+        df_temp = df['items'].apply(json_to_columns)
+        temp_frame = pd.concat([df, df_temp], axis=1)
+        temp_frame = temp_frame.drop(['items','items.product'], axis=1)
+        temp_frame.set_index('id', inplace=True)
+        return temp_frame
+
+    def products_by_page(self, page=1):
+        pages = '?page=' + str(page)
+        url = self.url + 'product' + pages + '&per-page=50'
+        response = requests.get(url=url, headers=self.headers)
+        print(response.status_code)
+        json_data = response.json()
+        df = pd.json_normalize(json_data)
+        self.pages = response.headers.get('X-Pagination-Page-Count')
+
+        # применение функции к датафрейму
+        temp_frame = df
+        temp_frame.set_index('id', inplace=True)
+        return temp_frame
+
+    def orders_req(self, start=1, end=10):
+        final_frame = pd.DataFrame()
+        for page in range(start, end+1):
+            final_frame = pd.concat([final_frame, self.orders_by_page(page=page)])
+        # final_frame['id']=final_frame.index
+        final_frame['shipments']=final_frame['shipments'].astype(str)
+        final_frame['services'] = final_frame['services'].astype(str)
+        return final_frame
+
+    def product_req(self, load_all=True, start=1, end=3):
+        final_frame = pd.DataFrame()
+        page = start
+        while page <= end:
+            final_frame = pd.concat([final_frame, self.products_by_page(page=page)])
+            if load_all and page == start:
+                end = int(self.pages)
+            page += 1
+        return final_frame
+
+
+# test = Ysell_regu()
+# test.orders_req()
+# test.product_req()
--- db_connect.py
+++ db_connect.py
@@ -1,9 +1,9 @@
 import datetime
-
+from Api_connect import Ysell_regu
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, insert, select, exists, inspect
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, Float, String, DATETIME, Date
+from sqlalchemy import Column, Integer, Float, String, DATETIME, Date, MetaData, Table, text
 import pandas as pd
 import sqlalchemy
 import numpy as np
@@ -11,7 +11,79 @@ import numpy as np
 # создаем сессию
 engine = create_engine('postgresql://data_user:12345678@localhost:5432/Aprobation')
 Session = sessionmaker(bind=engine)
+Base = sqlalchemy.orm.declarative_base()
+
+# session = Session()
+
+def create_table(table_name, columns):
+    # Создаем объект таблицы
+    metadata = MetaData()
+    table = Table(table_name, metadata)
+    inspector = inspect(engine)
+    for name, data_type in columns.items():
+        if name == 'id':
+            table.append_column(Column(name, data_type, primary_key=True))
+        else:
+            table.append_column(Column(name, data_type))
+    if not table_name in inspector.get_table_names():
+        metadata.create_all(engine)
+    return table
+
+
+def create_colums(frame=pd.DataFrame()):
+    columns_dict = {'id': Integer}
+    for col, dtype in frame.dtypes.items():
+        if dtype == 'int64':
+            columns_dict[col] = Integer
+        elif dtype == 'float64':
+            columns_dict[col] = Float
+        elif str(dtype).startswith('object'):
+            columns_dict[col] = String
+        else:
+            columns_dict[col] = String
+    return columns_dict
+
+
+data = Ysell_regu().orders_req(start=151, end=160)
+test = create_colums(data)
+table_name = 'orders'
+orders = create_table(table_name, test)
+
+
+
+
+class Orders(Base):
+    __tablename__ = table_name
+    __table__ = orders
+
+
 session = Session()
 
-# создаем класс Stats
-Base = sqlalchemy.orm.declarative_base()
+with engine.connect() as connection:
+    # Загрузить данные из датафрейма в список словарей
+    data['id']=data.index
+    data1 = data.to_dict('records')
+
+    # Обновить или добавить записи в таблицу
+    for row in data1:
+        orders = Orders(**row)
+        session.merge(orders)
+session.commit()
+session.close()
+with engine.connect() as connection:
+    result = connection.execute(text('SELECT * FROM ' + table_name))
+    df_result = pd.DataFrame(result.fetchall(), columns=result.keys())
+    df_result.to_excel('1234.xlsx')
+    print(df_result.tail())
+
+# закрытие соединения
+engine.dispose()
+# Session = sessionmaker(bind=engine)
+# session = Session()
+#
+# handbook = Handbook.__table__
+# select_statement = handbook.select()
+# result_set = session.execute(select_statement)
+#
+# for row in result_set:
+#     print(row)
