10b7eb9838c352f97767812b8d3b98e302ec30da
Davinchiam1 <ruslananushko2113@gmail.com>
2023-05-12 18:14:18 +0300

    googlsheets conector and small changes in db

--- frame_reader.py
+++ frame_reader.py
@@ -7,6 +7,8 @@ class Reports_loading:
     def __init__(self):
         self.inventory_frame = None
         self.reserved_frame = None
+        self.orders_frame = None
+        self.orders_list=[]
         self.inventory_list = []
         self.reserved_list = []
         self.result_frame = None
@@ -20,6 +22,9 @@ class Reports_loading:
                         self.inventory_list.append(f.path)
                     if 'Reserved' in str(f.path):
                         self.reserved_list.append(f.path)
+                elif f.is_file() and f.path.split('.')[-1].lower() == 'txt':
+                    if 'Order' in str(f.path):
+                        self.orders_list.append(f.path)
         else:
             for f in os.scandir():
                 if f.is_file() and f.path.split('.')[-1].lower() == 'csv':
@@ -27,26 +32,40 @@ class Reports_loading:
                         self.inventory_list.append(f.path)
                     if 'Reserved' in str(f.path):
                         self.reserved_list.append(f.path)
+                elif f.is_file() and f.path.split('.')[-1].lower() == 'txt':
+                    if 'Order' in str(f.path):
+                        self.orders_list.append(f.path)
 
     def _read_data(self):
         for file in self.inventory_list:
             temp_frame = pd.read_csv(file, delimiter=',',encoding='cp1252')
             self.inventory_frame = pd.concat([self.inventory_frame, temp_frame], ignore_index=True, axis=0)
         for file in self.reserved_list:
-            temp_frame_2 = pd.read_csv(file, delimiter=',',encoding='cp1252')
-            self.reserved_frame = pd.concat([self.reserved_frame, temp_frame_2], ignore_index=True, axis=0)
+            temp_frame = pd.read_csv(file, delimiter=',',encoding='cp1252')
+            self.reserved_frame = pd.concat([self.reserved_frame, temp_frame], ignore_index=True, axis=0)
+        for file in self.orders_list:
+            temp_frame = pd.read_csv(file, delimiter='\t', encoding='cp1252')
+            temp_frame=temp_frame[['sku','asin','item-status','quantity','currency','item-price']]
+            self.orders_frame=pd.concat([self.orders_frame, temp_frame], ignore_index=True, axis=0)
 
-    def _concentrate_data(self):
+    def _concentrate_data(self, status='Shipped'):
         """Concentrating data into one dataframe"""
         self.result_frame = pd.merge(self.inventory_frame, self.reserved_frame, on='fnsku', how='outer')
+        self.orders_frame = self.orders_frame[self.orders_frame['item-status'] == 'Shipped']
+        self.orders_frame = self.orders_frame.groupby('asin').agg({'quantity': 'sum', 'item-price': 'mean'})
+        self.orders_frame['avg_per_day']=self.orders_frame['quantity']/30
+        self.orders_frame['asin']=self.orders_frame.index
+
+        # self.result_frame=pd.merge(self.result_frame,self.orders_frame,left_on='asin_x',right_on='asin',how='outer')
 
     def get_data(self, directory=None):
         self._create_lists(directory=directory)
         self._read_data()
         self._concentrate_data()
-        return self.result_frame
+        return self.result_frame, self.orders_frame
 
 
-# test=Reports_loading()
-# frame=test.get_data(directory='./Amz Stock and Amz Orders')
-# frame.to_excel('test_file.xlsx', sheet_name='list1')
+test=Reports_loading()
+frame,frame2=test.get_data(directory='Z:\\Аналитика\\Amazon\\Update_api\\Amz Stock and Amz Orders')
+frame.to_excel('test_file.xlsx', sheet_name='list1')
+frame2.to_excel('test_file2.xlsx', sheet_name='list1')
--- google_connector.py
+++ google_connector.py
@@ -15,25 +15,36 @@ class Table_connest:
         client = gspread.authorize(self.credentials)
         self.table = client.open(table_name)
 
-    def load_frame(self, title, data_directory):
+    def load_frame(self, title1,title2, data_directory):
 
         try:
-            worksheet = self.table.worksheet(title=title)
+            worksheet = self.table.worksheet(title=title1)
         except gspread.WorksheetNotFound:
-            self.table.add_worksheet(title=title, rows="150", cols="60")
-            worksheet = self.table.worksheet(title)
+            self.table.add_worksheet(title=title1, rows="150", cols="60")
+            worksheet = self.table.worksheet(title1)
 
         loader = Reports_loading()
-        data = loader.get_data(data_directory)
-        data.fillna('0', inplace=True)
-        data = data[data.columns[1:].tolist() + [data.columns[0]]]
+        data_invres,data_ord = loader.get_data(data_directory)
+        data_invres.fillna('0', inplace=True)
+        data = data_invres[data_invres.columns[1:].tolist() + [data_invres.columns[0]]]
         data = [data.columns.values.tolist()] + data.values.tolist()
+        worksheet.update('A1', data)
+
+        try:
+            worksheet = self.table.worksheet(title=title2)
+        except gspread.WorksheetNotFound:
+            self.table.add_worksheet(title=title2, rows="150", cols="60")
+            worksheet = self.table.worksheet(title1)
+        columns=data_ord.columns.tolist()
+        columns = [columns[-1]] + columns[:-1]
+        data_ord=data_ord[columns]
+        data = [data_ord.columns.values.tolist()] + data_ord.values.tolist()
         worksheet.update('A1', data)
         print('Данные успешно загружены в таблицу.')
 
 
-table_connect = Table_connest(table_name='Копия Chews_stock_для аналитиков')
-table_connect.load_frame(title='Inv/Reserv', data_directory='Z:\\Аналитика\\Amazon\\Update_api\\Amz Stock and Amz Orders')
+table_connect = Table_connest(table_name='Chews_stock_best deal')
+table_connect.load_frame(title1='Inv/Reserv',title2='Ord', data_directory='Z:\\Аналитика\\Amazon\\Update_api\\Amz Stock and Amz Orders')
 # # Укажите путь к файлу ключа JSON
 # credentials = ServiceAccountCredentials.from_json_keyfile_name('test-table-386307-5f6f43257222.json', scope)
 #
